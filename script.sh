7db4de3baf354149effb22ec92ba6d3a78081c37
ThanhTran <thanh.tran@wisepass.co>
2019-08-21 08:23:56 +0700

    none

--- script.sh
+++ script.sh
@@ -1,3 +1,1 @@
-npm install
-pm2 del --silent itviec_30001
-pm2 start --silent server.js --name itviec_30001
+npm install pm2 del --silent itviec_30001 pm2 start --silent server.js --name itviec_30001
